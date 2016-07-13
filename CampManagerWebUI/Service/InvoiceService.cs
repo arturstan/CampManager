1ce5c901b487ea5ea4380d84f03404af059fe6ba
artur.stanaszek <arturstan@gmail.com>
2016-07-13 23:28:07 +0200

    Invoice edit - validate duplicate error

--- CampManagerWebUI/Service/InvoiceService.cs
+++ CampManagerWebUI/Service/InvoiceService.cs
@@ -36,15 +36,15 @@ namespace CampManagerWebUI.Service
 
         public void Edit(Invoice invoice, ref string error)
         {
-            //invoice.Number = invoice.Number.Trim();
-            //string number = invoice.Number;
-            //int idInvoiceEdit = invoice.Id;
-            //var invoiceExist = _db.Invoice.FirstOrDefault(x => x.Number == number && x.Id != idInvoiceEdit);
-            //if (invoiceExist != null)
-            //{
-            //    error = "Istnieje faktura o tym numerze";
-            //    return;
-            //}
+            invoice.Number = invoice.Number.Trim();
+            string number = invoice.Number;
+            int idInvoiceEdit = invoice.Id;
+            var invoiceExist = _db.Invoice.FirstOrDefault(x => x.Number == number && x.Id != idInvoiceEdit);
+            if (invoiceExist != null)
+            {
+                error = "Istnieje faktura o tym numerze";
+                return;
+            }
 
             _db.Entry(invoice).State = EntityState.Modified;
             _db.SaveChanges();
