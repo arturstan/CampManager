8439a7789f454f9a5a16eeeba6d7fd6a8a0154b1
Artur Stanaszek <arturstan@gmail.com>
2021-06-26 09:36:24 +0200

    Garbage model start

---
+++ CampManager.Domain/Domain/Garbage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampManager.Domain.Domain
+{
+    public class Garbage
+    {
+        public int Id { get; set; }
+
+        public SeasonOrganization Season { get; set; }
+
+        public DateTime Date { get; set; }
+
+
+    }
+}
